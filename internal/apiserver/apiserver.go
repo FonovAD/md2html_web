eaa3e655da41f8f4bcbafdf8816b345b000ef9a0
Alexandr Fonov <FonovAD@Gmail.com>
2024-03-27 19:41:49 +0300

    chore logger and router in apiserver.go

--- internal/apiserver/apiserver.go
+++ internal/apiserver/apiserver.go
@@ -1,15 +1,52 @@
 package apiserver
 
+import (
+	"fmt"
+	"net/http"
+
+	"github.com/sirupsen/logrus"
+)
+
 type APIserver struct {
 	config *Config
+	logger *logrus.Logger
+	router *http.ServeMux
 }
 
 func New(config *Config) *APIserver {
 	return &APIserver{
 		config: config,
+		logger: logrus.New(),
+		router: http.NewServeMux(),
 	}
 }
 
 func (s *APIserver) Start() error {
+	if err := s.ConfigureLogger(); err != nil {
+		return err
+	}
+
+	s.ConfigureRouter()
+	s.logger.Info("Starting API server")
+	return http.ListenAndServe(s.config.BindAddr, s.router)
+}
+
+func (s *APIserver) ConfigureLogger() error {
+	level, err := logrus.ParseLevel(s.config.LogLevel)
+	if err != nil {
+		return err
+	}
+	s.logger.SetLevel(level)
 	return nil
+}
+
+func (s *APIserver) ConfigureRouter() {
+	s.router.HandleFunc("/hello", s.HandleHello())
+}
+
+// test handle
+func (s *APIserver) HandleHello() http.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request) {
+		fmt.Fprintf(w, "Hello, %s", r.RemoteAddr)
+	}
 }
